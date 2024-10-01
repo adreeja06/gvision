6b93e13fd4a41a3c714b3632559ec93515e9dca1
Adreeja <adreejamahato@gmail.com>
2024-10-01 20:53:45 +0530

    Update Home component to use imported background image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Button from "../layouts/Button";
+import homeImage from "../assets/img/home.jpg"; // Import the image
 
 const Home = () => {
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('src/assets/img/home.jpg')] bg-no-repeat bg-cover opacity-90">
-      <div className=" w-full lg:w-4/5 space-y-5 mt-10">
+    <div
+      className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-no-repeat bg-cover opacity-90"
+      style={{ backgroundImage: `url(${homeImage})` }} // Use the imported image
+    >
+      <div className="w-full lg:w-4/5 space-y-5 mt-10">
         <h1 className="text-5xl font-bold leading-tight text-yellow">
-        TEAR-GUIDED NON-INVASIVE BLOOD GLUCOSE MONITORING USING OPTICAL INTELLIGENCE.
+          TEAR-GUIDED NON-INVASIVE BLOOD GLUCOSE MONITORING USING OPTICAL INTELLIGENCE.
           Empowering Health Choices for a Vibrant Life Your Trusted..
         </h1>
-      
-
         <Button title="Check your past records here" />
       </div>
     </div>
